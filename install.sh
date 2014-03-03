bcc3714ccae720280198e963c20260ba59e4c581
Carlo Eugster <carlo.eugster@gmail.com>
2014-03-02 18:32:15 -0800

    extended install script to copy dot files, added git to the brewfile

--- install.sh
+++ install.sh
@@ -1,7 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Colonize dem dots about the native system
 #
+
+echo "Backing up existing dot files"
+
+if [ ! -d ~/!olddots ]; then
+	mkdir ~/!olddots
+fi
+
+if [ -f ~/.bash_profile ]; then
+	mv ~/.bash_profile ~/!olddots
+fi
+cp .bash_profile ~/
+
+if [ -f ~/.bashrc ]; then
+	mv ~/.bashrc ~/!olddots
+fi
+cp .bashrc ~/
+
+if [ -f ~/.screenrc ]; then
+	mv ~/.screenrc ~/!olddots
+fi
+cp .screenrc ~/
+
+if [ -f ~/.vimrc ]; then
+	mv ~/.vimrc ~/!olddots
+fi
+cp .vimrc ~/
+
+if [ -f ~/.gitconfig ]; then
+	mv ~/.gitconfig ~/!olddots
+fi
+cp .gitconfig ~/
+
+if [ -f ~/.gitignore ]; then
+	mv ~/.gitignore ~/!olddots
+fi
+cp .gitignore ~/
 
 if test ! $(which brew)
 then
